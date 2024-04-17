ffe90bc3c253dcadffedbea7107cb5c04899780d
Leland Reardon <leland6925@gmail.com>
2024-04-16 20:33:27 -0700

    expose SideSheetEntry

--- lib/nested_side_sheet.dart
+++ lib/nested_side_sheet.dart
@@ -1,5 +1,6 @@
 library krootl_flutter_sheet;
 
 export 'package:nested_side_sheet/src/side_sheet_host.dart';
+export 'package:nested_side_sheet/src/side_sheet_entry.dart';
 export 'package:nested_side_sheet/src/side_sheet_position.dart';
 export 'package:nested_side_sheet/src/transitions/default_transitions.dart';
